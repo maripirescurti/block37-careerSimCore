325a30de13490496dbc0612329f5d8351b0a0078
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-10-13 23:28:05 -0400

    imported createTables to index.js and called

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 // import
-const { client } = require('./db');
+const { 
+  client,
+  createTables,
+ } = require('./db');
 
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
+  await createTables();
+  console.log('tables created');
 }
 
 init();
