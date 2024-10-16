bfd7e32670842b6422328714fffe0dc5ba7ebf18
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-10-16 03:29:45 -0400

    added NavBar.jsx component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,18 @@
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-  
+  return (
+    <nav className="navbar">
+      <div>
+        <h3>Your Pet Is My Boss</h3>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/register'>Register</Link></li>
+        <li><Link to='/account'>Account</Link></li>
+      </ul>
+    </nav>
+  );
 }
