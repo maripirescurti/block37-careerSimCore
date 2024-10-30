798d4d1c6ae9adcfa8e68e2c5afe2a6e826893b0
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-10-29 23:27:07 -0400

    test

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://docs.google.com/spreadsheets/d/112yozOXe1D1Pe7szDGjjYqtZ0KTNN9dcChZrBamn
 GitHub Project Board:
 https://github.com/users/maripirescurti/projects/5/views/1
 
+Deployed Site:
+https://luminous-queijadas-9cd15b.netlify.app/
--- server/index.js
+++ server/index.js
@@ -46,6 +46,16 @@ const {
 //  };
 //  app.use(cors(corsOptions));
 
+const cors = require('cors');
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow requests from your frontend
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+app.use(cors(corsOptions));
+
+
+
  // for deployment 
  const path = require('path');
  app.use(express.static(path.join(__dirname, 'build')));
